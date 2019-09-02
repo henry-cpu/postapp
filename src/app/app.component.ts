fd8253b1dfa1813f7cecbdf547ab5e5122ac361e
Harold Henry <haroldndefo@gmail.com>
2019-09-02 02:55:24 +0200

    update the article

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   articles = [
     {
-      title: 'Comment perdre 10kg en 2 heures',
-      content: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+      title: 'Comment perdre 10kg en 2 heures!',
+      content: 'Alors, ca marche, je l ai essayé moi meme! La recette est bien...',
       loveIts: 2,
     },
     {
-      title: 'Le surprenant secret de Angelina Jolie',
-      content: 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy',
+      title: 'Le surprenant secret de Angelina Jolie!',
+      content: 'Apres l immese succes du film Mr & Mme Smith, revelation choc...',
       loveIts: 0,
     },
     {
-      title: 'Devenir astronaute en 1 heure',
-      content: 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz',
+      title: 'Devenir astronaute en 1 heure!',
+      content: 'Vos vacances dans l espace sont prevu dans une semaine, mais ... ',
       loveIts: -1,
     }
   ];
